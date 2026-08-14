cd3f6dad983ec22b97b1efaa8360b6ea8254d2e4
Sampath <sampathmannam@gmail.com>
2026-08-14 12:04:01 +0530

    ﻿v1.4.4: bump versionCode 6 -> 7 and versionName 1.4.0 -> 1.4.4

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -41,8 +41,8 @@ android {
         applicationId = "com.baton.app"
         minSdk = 26
         targetSdk = 35
-        versionCode = 6
-        versionName = "1.4.0"
+        versionCode = 7
+        versionName = "1.4.4"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables { useSupportLibrary = true }
 
